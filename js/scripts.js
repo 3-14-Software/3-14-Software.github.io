f2af49c20af61f101ef317d5f9786ae51c760ebb
Christophe GERARDIN <cgerardin@elitemail.org>
2024-09-17 22:07:43 +0200

    Separated JS and gallery in lightbox

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+    document.getElementById('lightbox').addEventListener('click', function(e) {
+        if (e.target !== document.getElementById('lightbox-img')) {
+            closeLightbox();
+        }
+    });
+
+    if (window.innerWidth > 768) {
+    /*    setTimeout(function() {
+            document.querySelector('.popup').style = "";
+            document.querySelector('.popup').classList.add('fade-in');
+        }, 8000);
+    */
+    }
+    document.querySelector('header img').classList.add('fade-in');
+    setTimeout(function() {
+        document.querySelector('.description').classList.add('fade-in');
+    }, 300);
+    setTimeout(function() {
+        document.querySelector('.gallery').classList.add('fade-in');
+    }, 600);
+    setTimeout(function() {
+        document.querySelector('footer').classList.add('fade-in');
+    }, 900);
+};
+
+function openLightbox(src) {
+    document.getElementById('lightbox-img').src = src;
+    document.getElementById('lightbox').style.display = 'flex';
+}
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+}
